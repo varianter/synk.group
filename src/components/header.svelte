9cbaa27b30c59f15986f99a95edf3d27f6677692
Per Kristian W. Engebretsen <60431713+willandjs@users.noreply.github.com>
2023-04-30 14:02:39 +0200

    feat: setup project skeleton (#1)
    * Add Tailwind and icons, setup
    
    * Ignore yarn-error log
    
    * Import Tailwind
    
    * Setup project layout and initial header
    
    * chore: Setup prettier config, format files

---
+++ src/components/header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import UserIcon from '../components/icons/user.svelte';
+  import { MenuIcon } from 'svelte-feather-icons';
+  const date = new Date();
+  const options: any = { weekday: 'long', day: 'numeric', month: 'long' };
+  const formattedDate = date.toLocaleDateString('nb-NO', options);
+</script>
+
+<header class="p-8 grid items-center text-3xl">
+  <nav class="flex justify-between">
+    <div class="flex gap-x-12 items-center">
+      <button class="hover:bg-black/10 p-3 rounded-xl transition">
+        <MenuIcon size="20" />
+      </button>
+      <div><span class="font-medium">Synk.</span>Variant</div>
+    </div>
+
+    <div class="flex items-center gap-x-12">
+      <span>{formattedDate}</span>
+      <UserIcon />
+    </div>
+  </nav>
+</header>
