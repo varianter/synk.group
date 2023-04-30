c13fa3f19325d267545df2a42902735d6512538e
Per Kristian Engebretsen <per.engebretsen@lindbak.no>
2023-04-30 13:39:41 +0200

    Setup project layout and initial header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+
+	const balls = [
+		{ genreName: 'Rock', color: 'bg-red-500', dimensions: 'h-24 w-24' },
+		{ genreName: 'Rap', color: 'bg-blue-500', dimensions: 'h-80 w-80' },
+		{ genreName: 'Pop', color: 'bg-green-500', dimensions: 'h-24 w-24' },
+		{ genreName: 'Country', color: 'bg-slate-500', dimensions: 'h-40 w-40' },
+		{ genreName: 'Alternative', color: 'bg-yellow-500', dimensions: 'h-64 w-64' }
+	];
+</script>
+
+<section class="h-full grid items-end overflow-hidden">
+	<div class="flex items-end h-full w-full">
+		{#each balls as ball}
+			<div class="{ball.color} {ball.dimensions} rounded-full grid place-items-center">
+				{ball.genreName}
+			</div>
+		{/each}
+	</div>
+</section>
