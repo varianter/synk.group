dff72471bb68dc2b1b843a7af6193fc3cc12f3a3
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-06-08 23:44:10 +0200

    feat: login button spotify icon

--- src/lib/login/LoginHeader.svelte
+++ src/lib/login/LoginHeader.svelte
@@ -1,3 +1,7 @@
+<script>
+  import SpotifyIcon from '$lib/icons/SpotifyIcon.svelte';
+</script>
+
 <header class="grid items-center p-8">
   <nav class="flex justify-between">
     <div class="text-wonky flex items-center gap-x-8 text-2xl font-bold">
@@ -7,12 +11,15 @@
     <div class="flex items-center gap-x-4 text-lg font-semibold">
       <button
         class="grid h-12 place-content-center rounded-xl px-4 py-3 hover:bg-black hover:bg-opacity-[8%]"
-        >Hva er Synk?</button
       >
+        Hva er Synk?
+      </button>
       <button
-        class="grid h-12 place-content-center rounded-xl bg-black px-4 py-3 text-white transition-transform hover:scale-105"
-        >Logg inn</button
+        class="flex h-12 items-center gap-4 rounded-xl bg-black px-4 py-3 text-white transition-transform hover:scale-105"
       >
+        <SpotifyIcon size={24} />
+        <span>Logg inn</span>
+      </button>
     </div>
   </nav>
 </header>
